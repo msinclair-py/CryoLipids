158cc34b223af1ae5e79321347527d542c03ad7a
Matt Sinclair <msincla01@calcutta.ks.uiuc.edu>
2022-10-18 14:56:38 -0500

    integrated dihedral function

--- modeling.py
+++ modeling.py
@@ -33,10 +33,19 @@ class Lipid(PDB):
 
 
     @staticmethod
-    def measure_dihedral(a: np.ndarray, b: np.ndarray, 
-                         c: np.ndarray, d: np.ndarray) -> float:
-        v1, v2 = Lipid.vectorize(a, b, d, c)
-        return np.arccos(v1 @ v2)
+    def measure_dihedral(a: np.ndarray, b: np.ndarray,
+                      c: np.ndarray, d: np.ndarray) -> float:
+        v1, v2, v3 = Lipid.vectorize(b, a, c, b, d, c)
+        norm1 = np.cross(v1, v2)
+        norm2 = np.cross(v2, v3)
+        sign = np.sign(v1 @ v3)
+
+        rad = np.arccos(norm1 @ norm2)
+
+        if sign != 0:
+            rad *= sign
+    
+        return rad * 180 / np.pi
 
 
     @staticmethod
@@ -45,35 +54,3 @@ class Lipid(PDB):
         v1, v2, v3 = Lipid.vectorize(a, b, d, b, c, b)
         plane_normal = np.cross(v1, v2)
         return 90 - np.arccos(plane_normal @ v3)
-
-
-def calc_dihedral(u1, u2, u3, u4):
-    """ Calculate dihedral angle method. From bioPython.PDB
-    (adapted to np.array)
-    Calculate the dihedral angle between 4 vectors
-    representing 4 connected points. The angle is in
-    [-pi, pi].
-    """
-
-    a1 = u2 - u1
-    a2 = u3 - u2
-    a3 = u4 - u3
-
-    v1 = np.cross(a1, a2)
-    v1 = v1 / (v1 * v1).sum(-1)**0.5
-    v2 = np.cross(a2, a3)
-    v2 = v2 / (v2 * v2).sum(-1)**0.5
-    porm = np.sign((v1 * a3).sum(-1))
-    rad = np.arccos((v1*v2).sum(-1) / ((v1**2).sum(-1) * (v2**2).sum(-1))**0.5)
-    if not porm == 0:
-        rad = rad * porm
-
-    return rad
-
-a = np.array([1219.7, 4106.1, -7366.7])
-b = np.array([1277.1, 4016.6, -7447.1])
-c = np.array([1398.6, 3944.8, -7407.8])
-d = np.array([1501.2, 3943.2, -7521.3])
-
-alpha = calc_dihedral(a,b,c,d)
-
