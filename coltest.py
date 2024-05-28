e53dfbb173ea1650b7f5f3b30d315fbecf917915
msinclair-py <msinclair01940@gmail.com>
2024-05-28 16:18:55 -0500

    OccMap testing

--- coltest.py
+++ coltest.py
@@ -9,12 +9,14 @@ protein = np.array([[float(x) for x in line[31:54].strip().split()]
 lipid = np.array([[float(x) for x in line[31:54].strip().split()] 
                   for line in lines if 'POV' in line])
 
+lipid_lines = np.array([line for line in lines if 'POV' in line])
+
 occupancy_map = CollisionDetector(protein, lipid)
-linear_programming = CollisionDetector(protein, lipid, method=1)
+#linear_programming = CollisionDetector(protein, lipid, method=1)
 delaunay_triangulation = CollisionDetector(protein, lipid, method=2)
 #sparse_voxel_octree = CollisionDetector(protein, lipid, method=3)
 
-print(f'{occupancy_map.query_points()=}')
-print(f'{linear_programming.query_points()=}')
-print(f'{delaunay_triangulation.query_points()=}')
+print(f'{lipid_lines[occupancy_map.query_points()]=}')
+#print(f'{linear_programming.query_points()=}')
+print(f'{lipid_lines[delaunay_triangulation.query_points()]=}')
 #print(f'{sparse_voxel_octree.query_points()=}')
