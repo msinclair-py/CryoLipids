e573892c6d1541f00f0f209058cacb5d55863ea4
shastry2358 <ss171@illinois.edu>
2024-08-12 16:52:38 -0500

    Updating cryolipids.py in OpenMM branch

--- cryolipids.py
+++ cryolipids.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
-from collision_detection import CollisionDetector
-from minimizer import VacuumSimulator, ImplicitSolventSimulator
+from collision_detection import Repairer
+#from minimizer import VacuumSimulator, ImplicitSolventSimulator
 from modeling import Lipid
 from utilities import PDB
 import tomllib
@@ -11,9 +11,11 @@ config = tomllib.load(open('config.toml','rb'))
 structure = 'toy_models/model1.pdb'
 #
 
+lipids_to_model = [5] #[5, 6, 7, 11] # list of resids
+
 # read in cryo em model file and process
-pdb = PDB('misc/bmrcd.pdb', [5])
-pdb.write_to_pdb_file(pdb.contents)
+pdb = PDB('misc/bmrcd.pdb', lipids_to_model)
+pdb.write_to_pdb_file(pdb.contents) # writes initial lipid coords??
 
 # read in lipid(s) to be modeled
 lipid = Lipid(structure, 
@@ -23,23 +25,16 @@ lipid = Lipid(structure,
 # model lipid
 lipid.model()
 
-# check for protein conflict; octree?
-protein_coords = [[float(i) for i in line[32:54].strip().split()] 
+# check for protein conflicts
+protein_coords = [[float(i) for i in line[31:54].strip().split()] 
                   for line in pdb.protein]
 
-collision_detector = CollisionDetector(protein_coords, 
-                                       lipid, 
-                                       method=0)
-
-collisions = collision_detector.query_points()
-print(collisions)
-
-# fix conflict
-if any(collisions):
-    pass
+repair = Repairer(lipid, protein_coords, grid_spacing=1.5)
+repair.check_collisions()
+repair.write_pdb()
 
 # do vacuum minimization
-minimizer = VacuumSimulator(f'processed_{name}.pdb')
+#minimizer = VacuumSimulator(f'processed_{name}.pdb')
 
 # do implicit solvent minimization and relaxation
-minimizer = ImplicitSolventSimulator('vacuum_min.pdb')
+#minimizer = ImplicitSolventSimulator('vacuum_min.pdb')
